689d63c970cd4bdb6b23790544f5fa2d19b6d19a
LucasCordeiro-dev <lukacordeiro@hotmail.com>
2025-04-09 09:25:16 -0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia (numero:"123456", modelo: "M1",imei:"2222222",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("ZAP-ZAP");
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone (numero: "654321",modelo: "M2",imei:"1111111",memoria:120);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Safari");
